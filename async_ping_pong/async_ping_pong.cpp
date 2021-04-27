cbec506f5ddc055fb70c0f23b0b12b09241722fa
overflow0x17 <git_hub_mail@mail.ru>
2021-04-27 19:16:54 +0300

    v1

---
+++ async_ping_pong/async_ping_pong.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+constexpr int COUNTER = 50;
+std::mutex mtx;
+std::condition_variable check_print;
+bool is_printed = false;
+
+
+void ping()
+{
+	for (int i = 0; i < COUNTER; ++i)
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		check_print.wait(lock, []() {return !is_printed; });
+		std::cout << "ping\n";
+		is_printed = true;
+		check_print.notify_one();
+	}
+}
+
+void pong()
+{
+	for (int i = 0; i < COUNTER; ++i)
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		check_print.wait(lock, []() {return is_printed; });
+		std::cout << "pong\n";
+		is_printed = false;
+		check_print.notify_one();
+	}
+
+}
+int main()
+{
+	std::thread thread_1(ping);
+	std::thread thread_2(pong);
+
+
+	thread_1.join();
+	thread_2.join();
+}
